a07b8b4f3ce72ebdde4a2cd29ce32d648eea61e7
Ola Ishola <olaishola@hotmail.co.uk>
2022-11-24 11:00:33 +0100

    Add protected route to getting all users

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, Get, UseGuards } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import * as bcrypt from 'bcrypt';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('api/v1/auth')
 export class UsersController {
@@ -13,5 +14,12 @@ export class UsersController {
     CreateUserDto.password = hashedPassword;
     const userId = await this.UsersService.createUser(CreateUserDto);
     return 'User created successfully with and id: ' + userId;
+  }
+
+  @Get('users')
+  @UseGuards(AuthGuard())
+  async getUsers(): Promise<any> {
+    const users = await this.UsersService.getUsers();
+    return users;
   }
 }
