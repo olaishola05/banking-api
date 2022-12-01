7dcf6a79a325087d701f51ac4147793b802a19d6
Ola Ishola <olaishola@hotmail.co.uk>
2022-12-01 14:19:46 +0100

    Add remote origin

--- src/account/account.controller.spec.ts
+++ src/account/account.controller.spec.ts
@@ -1,18 +1,50 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AccountController } from './account.controller';
+import { AccountService } from './account.service';
 
 describe('AccountController', () => {
   let controller: AccountController;
+  let AccountService: AccountService;
+
+  // beforeEach(async () => {
+  //   const module: TestingModule = await Test.createTestingModule({
+  //     controllers: [AccountController],
+  //     providers: [AccountService],
+  //   }).compile();
 
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
+    const moduleRef = await Test.createTestingModule({
       controllers: [AccountController],
+      providers: [AccountService],
     }).compile();
 
-    controller = module.get<AccountController>(AccountController);
-  });
+    AccountController = moduleRef.get<AccountController>(AccountController);
+    AccountService = moduleRef.get<AccountService>(AccountService);
+  })
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  describe('createAccount', () => {
+    it('it should return Account created successfully', async () => {
+      const account = {
+        name: 'Test Account',
+        description: 'Test Account Description',
+        type: 'Test Account Type',
+        balance: 1000,
+      };
+
+      const user = {
+        id: '1',
+        token: 'test token',
+        role: 'test role',
+        expireIn: 'test expireIn',
+      }
+
+      const result: string = 'Account created successfully';
+
+      // jest.spyOn(AccountService, 'createAcct').mockImplementation(() =>c result);
+      // expect(await AccountController.createAcct(account, user)).toBe(result);
+    });
+
+    it('should be defined', () => {
+      expect(controller).toBeDefined();
+    });
   });
-});
